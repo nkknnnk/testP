c808aee639b378736e391bbd8c2da87f8dae466c
Nitish Kumar <nitish@infiniticube.in>
2024-02-13 11:59:02 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {MongoClient} = require("mongodb")
+
+const uri = 'mongodb://127.0.0.1'
+
+const client = new MongoClient(uri)
+
+const main = async () =>{
+await client.connect();
+const db = client.db("dbName: shop")
+const collection = db.collection("products")
+}
+
+main()
